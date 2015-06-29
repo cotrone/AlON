745e843708f49b30e45e83cb1883e67d6217b10f
davean <davean@xkcd.com>
2015-06-28 23:10:21 -0400

    Not even slightly working state in the middle of non-progress.

--- src/ALON/Source.hs
+++ src/ALON/Source.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables, FlexibleContexts #-}
 module ALON.Source (
-    DirTree
+    DirTree, DataUpdate(..)
   , dirSource
   ) where
 
@@ -38,20 +38,21 @@ data DataUpdate =
 dirSource :: MonadReflexCreateTrigger Spider m => TQueue (DSum (EventTrigger Spider)) -> FP.FilePath -> m (Event Spider [DataUpdate])
 dirSource q dir = do
   newEventWithTrigger $ \et -> do
-    t <- forkIO . FSN.withManagerConf (FSN.defaultConfig {FSN.confUsePolling = False}) $ \m -> do
-      eq <- newTQueueIO
-      -- Start listening for changes to the directory, feeding it to us via STM due to issues of isEmptyChan.
-      FSN.watchTree m dir (const True) (atomically . writeTQueue eq)
-      -- Get an initial read of the directory, and issues with read correctness will be covered by inotify
-      ifs <- liftIO readDb
-      pre <- atomically . whileM (not <$> isEmptyTQueue eq) $ readTQueue eq
-      -- We have to convert the stable read to update events, overlay the changes as events, and emit a big event.
-      pres <- mapM e2e pre
-      atomically . writeTQueue q $ et :=> pres
-      -- Then we just watch the changes, and send them on
-      forever . E.handle (\(e::E.SomeException) -> print e) $ do
-        e <- atomically (readTQueue eq) >>= e2e
-        atomically . writeTQueue q $ et :=> [e]
+    t <- forkIO . FSN.withManagerConf (FSN.defaultConfig {FSN.confUsePolling = False}) $ \m ->
+      E.handle (\(e::E.SomeException) -> putStrLn ("Excp: "++show e)) $ do
+        eq <- newTQueueIO
+        -- Start listening for changes to the directory, feeding it to us via STM due to issues of isEmptyChan.
+        FSN.watchTree m dir (const True) (atomically . writeTQueue eq)
+        -- Get an initial read of the directory, and issues with read correctness will be covered by inotify
+        ifs <- liftIO readDb
+        pre <- atomically . whileM (not <$> isEmptyTQueue eq) $ readTQueue eq
+        -- We have to convert the stable read to update events, overlay the changes as events, and emit a big event.
+        pres <- mapM e2e pre
+        atomically . writeTQueue q $ et :=> pres
+        -- Then we just watch the changes, and send them on
+        forever . E.handle (\(e::E.SomeException) -> putStrLn ("Excp: "++show e)) $ do
+          e <- atomically (readTQueue eq) >>= e2e
+          atomically . writeTQueue q $ et :=> [e]
     return (killThread t)
   where
     e2e :: FSN.Event -> IO DataUpdate
