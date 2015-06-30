7894016a448d9bcf6b6062e8aad0d564860834ec
davean <davean@xkcd.com>
2015-06-30 11:14:27 -0400

    Now with actual FRP usage.
    Sadly we have to sample seperately from using fireEventsAndRead currently.
    Need to understand fireEventsAndRead better.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, FlexibleContexts #-}
 module Main where
 
 import Control.Monad
@@ -6,27 +6,41 @@ import Control.Monad.Trans
 import ALON.Source
 import Reflex
 import Reflex.Host.Class
+import Data.Dependent.Sum (DSum ((:=>)))
 import Reflex.Spider
 import Reflex.Spider.Internal (ResultM)
+import Data.Time
+import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TQueue
 
-timePulse :: MonadReflexCreateTrigger Spider m => TQueue (DSum (EventTrigger Spider)) -> DiffTime -> m (Event Spider UTCTime)
-timePulse q dt = 
+import Data.Maybe
+    
+timePulse :: (MonadIO m, MonadReflexCreateTrigger Spider m) => TQueue (DSum (EventTrigger Spider)) -> DiffTime -> m (Event Spider UTCTime)
+timePulse q dt = do
+  liftIO . putStrLn $ "Time event source asked for."
+  newEventWithTrigger $ \et -> do
+    putStrLn "Time event source started!"
+    t <- forkIO . forever $ do
+           getCurrentTime >>= (atomically . writeTQueue q . (et :=>))
+           threadDelay . floor $ dt*(10^6)
+    return (putStrLn "Time event source killed!" >> killThread t)
 
 main :: IO ()
 main = runSpiderHost $ do
   eq <- liftIO newTQueueIO
 
-  de <- dirSource eq "test_dir"
+  --de <- dirSource eq "test_dir"
 
   o <- runHostFrame $ do
-    
-    return de
+    et <- timePulse eq 1
+    now <- liftIO getCurrentTime
+    hold now et
 
   forever $ do
     e <- liftIO . atomically $ readTQueue eq
-    r <- fireEventsAndRead [e] (return ())
+    fireEventsAndRead [e] (return ())
+    r <- sample o
     liftIO $ print r
 
   return ()
