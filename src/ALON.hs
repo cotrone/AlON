d38135ec9cea21be0f4245c4aa26b5cd73fb3870
davean <davean@xkcd.com>
2016-11-17 19:00:36 -0500

    Add a global ALON export module to make importing ALON easier.

---
+++ src/ALON.hs
@@ -0,0 +1,13 @@
+module ALON (
+   module ALON.Source
+ , module ALON.Manipulation
+ , module ALON.Run
+ , module ALON.WebServer
+ , module ALON.Transforms
+ ) where
+
+import ALON.Source
+import ALON.Manipulation
+import ALON.Run
+import ALON.WebServer
+import ALON.Transforms
