c99f7b39c22ea5b344534727440eaac4ea992373
davean <davean@xkcd.com>
2022-03-17 19:13:57 -0400

    FUCK YOU NIX FLAKES, also ...

--- flake.nix
+++ flake.nix
@@ -1,9 +1,11 @@
 {
   description = "AlON";
+
+  inputs.nixpkgs.follows = "nixpkgs-unstable";
+  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+  outputs = { self, nixpkgs, flake-utils, haskellNix, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
     let
       overlays = [ haskellNix.overlay
